fa3bfe869ef00c6a409a19b02f9d5f5c143f2d2b
Praneet6 <praneetrathore6@gmail.com>
2026-03-20 15:24:06 +0530

    Praneet6 Added feature/UC5-BookingQueue

---
+++ APP/src/BookingRequestQueue.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * CLASS - BookingRequestQueue
+ * Use Case 5: Booking Request (FIFO)
+ */
+public class BookingRequestQueue {
+
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
+
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
+
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
+    }
+}
